c7885095d9131ff1905699486a991d813a0404bf
Horodetskyi Vitalii <23rash23@gmail.com>
2022-05-21 22:19:53 +0300

    hm-24 added after fix

--- hm-24/app/index.js
+++ hm-24/app/index.js
@@ -1,5 +1,3 @@
-// симулятор ошибок
-
 const getRandomInt = (min, max) => {
 	min = Math.ceil(min);
 	max = Math.floor(max);
@@ -16,78 +14,91 @@ const randomizeError = () => {
 	return null;
 };
 
-// симулятор запроса в БД в таблицу юзеров.
-const getUsers = async (callback) => {
-	const USERS = [
-		{ id: 1, name: 'Bob' },
-		{ id: 2, name: 'Andy' },
-		{ id: 3, name: 'John' },
-	];
+const getUsers = () => {
+	return new Promise((resolve, reject) => {
+		const USERS = [
+			{ id: 1, name: 'Bob' },
+			{ id: 2, name: 'Andy' },
+			{ id: 3, name: 'John' },
+		];
 
-	setTimeout(() => {
-		callback(randomizeError(), USERS);
-	}, 2000);
+		setTimeout(() => {
+			const err = randomizeError();
+			if (err === null) {
+				resolve(USERS);
+			} else {
+				reject(err);
+			}
+		}, 2000);
+	});
 };
 
-// симулятор запроса в таблицу продуктов.
-const getProducts = async (callback) => {
-	const PRODUCTS = [
-		{ id: 1, name: 'iPad' },
-		{ id: 2, name: 'Google Pixel' },
-		{ id: 3, name: 'War and Peace' },
-		{ id: 4, name: 'iPad' },
-		{ id: 5, name: 'Kaizen' },
-		{ id: 6, name: 'Sherlock Holmes' },
-	];
+const getOrders = () => {
+	return new Promise((resolve, reject) => {
+		const ORDERS = [
+			{ id: 1, userId: 1, checkout: [1, 6] },
+			{ id: 2, userId: 1, checkout: [3] },
+			{ id: 3, userId: 2, checkout: [2, 4] },
+		];
 
-	setTimeout(() => {
-		callback(randomizeError(), PRODUCTS);
-	}, 2000);
+		setTimeout(() => {
+			const err = randomizeError();
+			if (err === null) {
+				resolve(ORDERS);
+			} else {
+				reject(err);
+			}
+		}, 2000);
+	});
 };
 
-// симулятор запроса в таблицу заказов.
-const getOrders = async (callback) => {
-	const ORDERS = [
-		{ id: 1, userId: 1, checkout: [1, 6] },
-		{ id: 2, userId: 1, checkout: [3] },
-		{ id: 3, userId: 2, checkout: [2, 4] },
-	];
+const getProducts = () => {
+	return new Promise((resolve, reject) => {
+		const PRODUCTS = [
+			{ id: 1, name: 'iPad' },
+			{ id: 2, name: 'Google Pixel' },
+			{ id: 3, name: 'War and Peace' },
+			{ id: 4, name: 'iPad' },
+			{ id: 5, name: 'Kaizen' },
+			{ id: 6, name: 'Sherlock Holmes' },
+		];
 
-	setTimeout(() => {
-		callback(randomizeError(), ORDERS);
-	}, 2000);
+		setTimeout(() => {
+			const err = randomizeError();
+			if (err === null) {
+				resolve(PRODUCTS);
+			} else {
+				reject(err);
+			}
+		}, 2000);
+	});
 };
 
 const getCheckoutsForUserAsPromise = async (userId) => {
+	let order;
+	let result;
 	try {
-		let result;
-		let order;
-		await getUsers((error, users) => {
-			if (error === null) {
-				const user = users.find((user) => user.id === userId);
+		const users = await getUsers();
+		const user = users.find((user) => user.id === userId);
 
-				result = user;
-			}
-		});
+		console.log(user);
+		result = user;
 
-		await getOrders((error, orders) => {
-			if (error === null) {
-				order = orders.filter((order) => order.userId === userId);
-			}
-		});
+		const orders = await getOrders();
+		order = orders.filter((order) => order.userId === userId);
 
-		await getProducts((error, products) => {
-			if (error === null) {
-				let product = order.map((e) => ({
-					...e,
-					checkout: e.checkout.map((c) => products.find((k) => k.id === c)),
-				}));
+		console.log(order);
 
-				result.orders = product;
-			}
+		const products = await getProducts();
+		let product = order.map((e) => ({
+			...e,
+			checkout: e.checkout.map((c) => products.find((k) => k.id === c)),
+		}));
 
-			console.log(result);
-		});
+		console.log(product);
+		result.orders = product;
+
+		console.log(result);
 	} catch (error) {
 		console.error(error);
 	}
